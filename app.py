eb233b584c35ad07ae91499aec2f3b220d31eb79
MartinRojo1 <martinrojoandres1@gmail.com>
2023-07-02 13:48:39 -0300

    Creacion de apertura de ruleta

--- app.py
+++ app.py
@@ -10,13 +10,16 @@ db = mysql.connector.connect(
     password='password',
     database='mezumo'
 )
+
 cursor = db.cursor()
 
 
 # Crear una instancia con FastAPI
 app = Flask(__name__)
 
+##########################################################################
 
+# Endpoint de creacion de ruleta
 @app.route('/ruletas', methods=['POST'])
 def crear_ruleta():
 
@@ -36,9 +39,39 @@ def crear_ruleta():
     
     return jsonify({'ruleta_id': ruleta_id})
 
+##########################################################################
+
+# Endpoint de apertura de ruleta.
+@app.route('/ruletas/<ruleta_id>/apertura', methods=['POST'])
+def abrir_ruleta(ruleta_id):
+    
+    '''
+    Esta funcion se encarga de abrir la ruleta y devolver si fue abierta con exito o no.
+
+    '''
+
+    # Buscar el id en la tabla ruletas
+    query = "SELECT estado FROM ruletas WHERE id = %s"
+    cursor.execute(query, (ruleta_id,))
+    ruleta = cursor.fetchone()
+
+    # Generar la apertura de la ruleta si es que existe y que este en estado igual a 0.
+    if ruleta and ruleta[0] == 0:
+
+        # Update al estado de la ruleta
+        query_estado = "UPDATE ruletas SET estado = 1 WHERE id = %s"
+        cursor.execute(query_estado, (ruleta_id,))
+        db.commit()
+
+        return jsonify({'estado': 'Apertura exitosa'})
+    
+    else:
+
+        return jsonify({'estado': 'Apertura denegada'})
+
 
 
 
 # Iniciar el servidor de desarrollo uvicorn
 if __name__ == "__main__":
-    app.run(debug=True, port=3006)
+    app.run(debug=True, port=3007)
